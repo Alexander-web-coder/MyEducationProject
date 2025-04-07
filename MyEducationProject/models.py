7e61543fd24ff9320e9397f13a24ff4abb792f6e
Alexander-web-coder <shurikl@list.ru>
2025-04-07 19:58:05 +0400

    Подключена база. Добавлена иконка. Сделан вывод информации в таблицу из базы. Сделан вывод статистики из базы с учетом только поля description.

---
+++ MyEducationProject/models.py
@@ -0,0 +1,13 @@
+# Create your models here.
+from django.db import models
+
+
+class Term(models.Model):
+    id = models.AutoField(primary_key=True, null=False)
+    m_term = models.TextField(blank=False, null=False)
+    description = models.TextField(blank=True, null=True)
+    author = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = True
+        db_table = 'music_terms'
